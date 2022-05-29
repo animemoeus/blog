1d357f206b9f59e2e2972b4a6a7b472201633f51
Arter Tendean <arter.tendean.07@gmail.com>
2022-05-29 19:34:23 +0800

    adding Navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Navbar from "../components/molecules/Navbar";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
