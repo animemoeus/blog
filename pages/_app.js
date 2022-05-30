66310f716f33d02e5ac6590acfd20dbd5c57bce0
Arter Tendean <arter.tendean.07@gmail.com>
2022-05-30 14:58:18 +0800

    adding nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import "nprogress/nprogress.css";
 import "../styles/globals.css";
+
+import NProgress from "nprogress";
+import Router from "next/router";
 import { ChakraProvider } from "@chakra-ui/react";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
   return (
