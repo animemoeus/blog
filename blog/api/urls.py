b52b1dce2c292b597c18965e016af053a5266f43
Arter Tendean <arter.tendean.07@gmail.com>
2022-05-30 08:58:00 +0800

    addin blog post detail API

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("posts/", views.posts),
+    path("post/<slug:slug>/", views.detail),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework import status
 
 from blog.models import Post
 from .serializers import PostSerializer
@@ -13,7 +14,7 @@ def index(request):
 
 @api_view()
 def posts(request):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by("-id")
 
     paginator = PageNumberPagination()
     paginator.page_size = 7
@@ -22,3 +23,17 @@ def posts(request):
     serializer = PostSerializer(result, many=True)
 
     return paginator.get_paginated_response(serializer.data)
+
+
+@api_view()
+def detail(request, slug):
+    try:
+        queryset = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(
+            {"success": False, "data": None}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = PostSerializer(queryset)
+
+    return Response({"success": True, "data": serializer.data})
