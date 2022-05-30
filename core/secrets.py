b3c964f9fd0d75b8010e1dd9b7bc44051126a1b4
Arter Tendean <arter.tendean.07@gmail.com>
2022-05-30 18:34:48 +0800

    Create secrets.py

---
+++ core/secrets.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "database_name",
+        "USER": "database_user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "OPTIONS": {"charset": "utf8mb4"},
+    },
+}
+
+SECRET_KEY = "random-string-bla-bla-bla"
